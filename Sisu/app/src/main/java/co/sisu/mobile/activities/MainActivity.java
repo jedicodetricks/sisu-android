87bdcd8dce0b2ece95a75f5044fdc1bbff2ea2ad
Brady Groharing <bradygroharing@Bradys-MacBook-Pro.local>
2018-02-14 23:32:05 -0700

    Added enough to the main to get us able to navigate into the app

--- Sisu/app/src/main/java/co/sisu/mobile/activities/MainActivity.java
+++ Sisu/app/src/main/java/co/sisu/mobile/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package co.sisu.mobile.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import co.sisu.mobile.R;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupSignInButton();
     }
+
+    private void setupSignInButton() {
+        Button signInButton = findViewById(R.id.signInButton);
+        final EditText emailAddress = findViewById(R.id.emailInput);
+        final EditText password = findViewById(R.id.passwordInput);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    private void attemptLogin() {
+        Intent intent = new Intent(this, ScoreboardActivity.class);
+        startActivity(intent);
+
+    }
+
 
 }
