c9b1b4ba4458d82d8f8ec2c6d3e21bce4583ba69
Brady Groharing <bradygroharing@STGAdmins-MacBook-Pro-2.local>
2018-05-02 16:33:42 -0600

    small bug fixes

--- Sisu/app/src/main/java/co/sisu/mobile/fragments/ClientListFragment.java
+++ Sisu/app/src/main/java/co/sisu/mobile/fragments/ClientListFragment.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -112,7 +111,7 @@ public class ClientListFragment extends Fragment implements AdapterView.OnItemCl
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         ClientObject selectedClient = (ClientObject) parent.getItemAtPosition(position);
         parentActivity.setSelectedClient(selectedClient);
-        parentActivity.stackReplaceFragment(ClientFragment.class);
+        parentActivity.stackReplaceFragment(ClientEditFragment.class);
         parentActivity.swapToBacktionBar(null);
     }
 
