f90e9bdcb42db8ec928bc6529424130403dc40cf
jedicodetricks <jeffjessop@outlook.com>
2018-05-07 10:41:14 -0600

    updated all reports to show correctly

--- Sisu/app/src/main/java/co/sisu/mobile/adapters/ReportListAdapter.java
+++ Sisu/app/src/main/java/co/sisu/mobile/adapters/ReportListAdapter.java
@@ -72,26 +72,34 @@ public class ReportListAdapter extends BaseAdapter {
         Metric metric = (Metric) getItem(position);
 
         titleTextView.setText(metric.getTitle());
-
-        switch(metric.getTitle()) {
-            case "Listing Showings":
-                subtitleTextView.setText(metric.getCurrentNum());
-                percentageTextView.setVisibility(View.INVISIBLE);
-                progressBar.setVisibility(View.INVISIBLE);
-            break;
-            default:
-                subtitleTextView.setText(metric.getCurrentNum() + " of " + metric.getGoalNum());
-                percentageTextView.setText(metric.getPercentComplete() + "% complete");
-                progressBar.setProgress(metric.getPercentComplete());
-                progressBar.setScaleY(4f);
-
-                int color = metric.getColor() != 0 ? ContextCompat.getColor(mContext, R.color.colorMoonBlue) : ContextCompat.getColor(mContext, metric.getColor());
-                progressBar.setProgressTintList(ColorStateList.valueOf(color));
-                animateBars(progressBar);
-            break;
+        if(metric.getCurrentNum() < 0) {
+            metric.setCurrentNum(0);
         }
+        if(metric.getTitle().equalsIgnoreCase("listing showings") ||
+                metric.getTitle().equalsIgnoreCase("number of dials") ||
+                metric.getTitle().equalsIgnoreCase("thank you cards") ||
+                metric.getTitle().equalsIgnoreCase("open houses") ||
+                metric.getTitle().equalsIgnoreCase("added to database") ||
+                metric.getTitle().equalsIgnoreCase("referrals requested") ||
+                metric.getTitle().equalsIgnoreCase("referrals received") ||
+                metric.getTitle().equalsIgnoreCase("prepared cma") ||
+                metric.getTitle().equalsIgnoreCase("meditate") ||
+                metric.getTitle().equalsIgnoreCase("hours prospected") ||
+                metric.getTitle().equalsIgnoreCase("exercise")) {
 
+            subtitleTextView.setText("Total: " + metric.getCurrentNum());
+            percentageTextView.setVisibility(View.INVISIBLE);
+            progressBar.setVisibility(View.INVISIBLE);
+        } else {
+            subtitleTextView.setText(metric.getCurrentNum() + " of " + metric.getGoalNum());
+            percentageTextView.setText(metric.getPercentComplete() + "% complete");
+            progressBar.setProgress(metric.getPercentComplete());
+            progressBar.setScaleY(4f);
 
+            int color = metric.getColor() != 0 ? ContextCompat.getColor(mContext, R.color.colorMoonBlue) : ContextCompat.getColor(mContext, metric.getColor());
+            progressBar.setProgressTintList(ColorStateList.valueOf(color));
+            animateBars(progressBar);
+        }
         thumbnailImageView.setImageResource(metric.getThumbnailId());
 
         return rowView;
