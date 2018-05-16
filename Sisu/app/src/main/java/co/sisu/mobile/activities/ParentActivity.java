e4fec0e3c6a3950a3937dbd57299cbb923d6d6cc
Brady Groharing <bradygroharing@STGAdmins-MacBook-Pro-2.local>
2018-05-16 12:52:36 -0600

    no right swipe for you

--- Sisu/app/src/main/java/co/sisu/mobile/activities/ParentActivity.java
+++ Sisu/app/src/main/java/co/sisu/mobile/activities/ParentActivity.java
@@ -134,6 +134,8 @@ public class ParentActivity extends AppCompatActivity implements View.OnClickLis
 
 
     public void initializeActionBar() {
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
+
         bar.setCustomView(R.layout.action_bar_layout);
         pageTitle = findViewById(R.id.action_bar_title);
         teamLetter = findViewById(R.id.team_letter);
@@ -147,6 +149,7 @@ public class ParentActivity extends AppCompatActivity implements View.OnClickLis
         homeButton.setOnClickListener(this);
 
         if(teamsList != null && pageTitle.getText().toString().equals("Leaderboard")) {
+            drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
             teamBlock.setBackgroundColor(teamsList.get(selectedTeam).getColor());
             teamLetter.setText(teamsList.get(selectedTeam).getTeamLetter().toUpperCase());
             teamLetter.setBackgroundColor(teamsList.get(selectedTeam).getColor());
